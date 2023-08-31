80ad6bfa2ad8070ed78adc1279b3466d6b997a3a
sadia11-a <sadiasorna96@gmail.com>
2023-08-31 23:48:37 +0600

    blog button done

---
+++ blog.js
@@ -0,0 +1,4 @@
+document.getElementById("btn-blog").addEventListener("click", function () {
+  //   console.log("clicked");
+  window.location.href = "qus.html";
+});
---
+++ index.js
@@ -0,0 +1,8 @@
+const handleCategory = async () => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await response.json();
+  console.log(data.data.filter());
+};
+handleCategory();
