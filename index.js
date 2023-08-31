d22fbf9449e00b8e08b9ee163cacc1378ae3c1f1
sadia11-a <sadiasorna96@gmail.com>
2023-09-01 01:40:22 +0600

    card start here

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const handleCategory = async () => {
     "https://openapi.programming-hero.com/api/videos/categories"
   );
   const data = await response.json();
-  console.log(data.data.filter());
+  const btnContainer = document.getElementById("btn-container");
+  data.data.forEach((category) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <button onclick=" handleLoadNews('${category.category_id}')" class="btn">${category.category}</button>
+    
+    `;
+    btnContainer.appendChild(div);
+  });
+  console.log(data.data);
 };
+const handleLoadNews = async (categoryId) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
+  );
+  const data = await response.json();
+  console.log(data.data);
+};
+
 handleCategory();
