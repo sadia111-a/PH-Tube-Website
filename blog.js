5a10d742d817319019422a3cc9577391de98f5ad
sadia11-a <sadiasorna96@gmail.com>
2023-09-02 01:50:43 +0600

    all done without sorting

--- blog.js
+++ blog.js
@@ -4,3 +4,20 @@ document.getElementById("btn-blog").addEventListener("click", function () {
   newTabUrl = "qus.html";
   window.open(newTabUrl, "_blank");
 });
+
+// const div = document.createElement("div");
+// div.innerHTML = `
+//   <div class="text-center">
+//           <img src="./Icon.png" alt="" />
+//           <p class="text-center text-3xl">
+//             Oops!! Sorry, There is no content here
+//           </p>
+//         </div>
+//   `;
+// cardContainer.appendChild(div);
+
+// if (data.data.length === 0) {
+//     cardContainer.classList.add("hidden");
+//   } else {
+//     cardContainer.classList.remove("hidden");
+//   }
